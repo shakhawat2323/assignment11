666009beb7c33ff49f12f47d1698e8dea33b8fa9
shakhawat2323 <shakhawat2323>
2025-02-06 01:03:12 +0600

    my project 85% done

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Bannar from "./Bannar";
 import Artifacthomecard from "./Artifacthomecard";
+import Newsection from "./Newsection";
 
 const Home = () => {
   return (
     <div>
       <Bannar></Bannar>
       <Artifacthomecard></Artifacthomecard>
+      <Newsection></Newsection>
     </div>
   );
 };
