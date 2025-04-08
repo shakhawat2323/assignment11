aa37e3a6a7f48e55c448e6478658a5c50fa72318
shakhawat2323 <shakhawathossain208@gmail.com>
2025-04-08 15:15:19 +0600

    server link change

--- src/Component/Pages/MyLikedArtifacts.jsx
+++ src/Component/Pages/MyLikedArtifacts.jsx
@@ -1,53 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import UseAuth from "../Hook/UseAuth";
-// import axios from "axios";
-// import LickCounttable from "./LickCounttable";
-// import { FcLike } from "react-icons/fc";
-
-// const MyLikedArtifacts = () => {
-//   const { user } = UseAuth();
-
-//   const [artifact, SetArtifact] = useState([]);
-
-//   useEffect(() => {
-//     mypostartifact();
-//   }, [user]);
-
-//   const mypostartifact = async () => {
-//     const { data } = await axios.get(
-//       `${import.meta.env.VITE_SOME_KEY}/arifacts/${user?.email}`
-//     );
-//     SetArtifact(data);
-//   };
-//   console.log(artifact);
-//   return (
-//     <div className="w-11/12 mx-auto">
-//       <h1 className="text-4xl font-bold text-center mt-5">this is my jobes</h1>
-//       <div className="overflow-x-auto">
-//         <table className="table">
-//           {/* head */}
-//           <thead>
-//             <tr>
-//               <th>Artifact Name</th>
-//               <th>description</th>
-//               <th>email</th>
-//               <th className="flex items-center gap-2">
-//                 Lick <FcLike></FcLike>
-//               </th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {artifact.map((Licke, index) => (
-//               <LickCounttable Licke={Licke} key={index}></LickCounttable>
-//             ))}
-//           </tbody>
-//         </table>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MyLikedArtifacts;
 import React, { useEffect, useState } from "react";
 import UseAuth from "../Hook/UseAuth";
 import axios from "axios";
@@ -59,7 +9,7 @@ const MyLikedArtifacts = () => {
   const { user } = UseAuth();
   const [artifact, setArtifact] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 8; // প্রতি পেজে ৫টি ডাটা দেখাবে
+  const itemsPerPage = 8;
 
   useEffect(() => {
     if (user?.email) {
@@ -72,6 +22,7 @@ const MyLikedArtifacts = () => {
       const { data } = await axios.get(
         `${import.meta.env.VITE_SOME_KEY}/arifacts/${user?.email}`
       );
+
       setArtifact(data);
     } catch (error) {
       console.error("Error fetching liked artifacts:", error);
